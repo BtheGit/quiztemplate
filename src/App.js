bbd8b0dfd9d9393cabdf02ee98ec6b904bad2ab5
BtheGit <brendancommerce@gmail.com>
2016-12-19 17:06:27 +0800

    Added Stashthis.com widget + react-helmet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import { RouteTransition } from 'react-router-transition';
+import Helmet from 'react-helmet';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Helmet 
+          titleTemplate="WhatAmI-Quiz.com - %s"
+          defaultTitle='"What am I, really?" Find the real you with a few simple questions.'
+          script={[
+            {"type": "text/javascript", "innerHTML": `{var switchTo5x=true;}`},           
+            {"type": "text/javascript", "src": "http://w.sharethis.com/button/buttons.js?publisher=61f9d1b3-4729-4b86-a17f-cfcb51980324"},
+            {"type": "text/javascript", "innerHTML": `{stLight.options({publisher: "61f9d1b3-4729-4b86-a17f-cfcb51980324", doNotHash: false, doNotCopy: false, hashAddressBar: true});}`}
+          ]}          
+        />
         <RouteTransition
           pathname={this.props.location.pathname}
           className="transition-wrapper"
--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AdUnit from '../components/AdUnit/AdUnit';
+import Helmet from 'react-helmet';
 
 function ResultsPage (props) {
 
@@ -19,9 +20,28 @@ function ResultsPage (props) {
 
 	return (
 		<div>
+			<div className="resultsHelmet">
+				<Helmet 
+					title={result}
+					titleTemplate="WhatAmI-Quiz.com - %s"
+					defaultTitle='"What am I, really?" Find the real you with a few simple questions.'
+
+				/>
+			</div>
 			<h1>Your result</h1>
 			<h2>{result}</h2>
 			<AdUnit type="336x280"></AdUnit>
+			<div>
+				<span className='st_facebook_vcount'></span>
+				<span className='st_twitter_vcount'></span>
+				<span className='st_pinterest_vcount'></span>
+				<span className='st_linkedin_vcount'></span>
+				<span className='st_plusone_vcount'></span>
+				<span className='st_tumblr_vcount'></span>
+				<span className='st_pocket_vcount'></span>
+				<span className='st_sharethis_vcount'></span>
+				<span className='st_email_vcount'></span>
+			</div>
 		</div>
 	);
 }
